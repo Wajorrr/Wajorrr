40d3aa782526bc7e729c13dda05beca94d713a25
wajorrr <2434007515@qq.com>
2022-08-03 16:56:54 +0800

    test

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div style="display:flex;flex-direction: column;">
-        121243124123124
+        push test
         <Header ref="header" @submit-data2="getData2" @submit-data="getData" :data="list" title="这是头部(父组件传递)"></Header>
         <div class="container">
             <div class="container-left">
